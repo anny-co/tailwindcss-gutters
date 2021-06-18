58182794da99f5f7487401f45aaa5bb9c6679820
Alexander Bartolomey <occloxium@gmail.com>
2021-06-18 23:39:39 +0200

    deps: just import small lodash deps
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- index.js
+++ index.js
@@ -1,72 +1,74 @@
 const plugin = require("tailwindcss/plugin");
-const { fromPairs, concat, map } = require("lodash");
-module.exports = plugin(
-  function ({ theme, variants, addUtilities, e }) {
-    const guttersTheme = theme("gap");
-    const guttersVariants = variants("gap");
+const fromPairs = require("lodash.frompairs");
+const concat = require("lodash.concat");
+const map = require("lodash.map");
 
-    addUtilities({
-      [`.${e("gutters-wrapper")}`]: {
-        display: "flow-root",
-      },
-      [`.${e("gutters-wrapper")}::before, .${e("gutters-wrapper")}::after`]: {
-        content: "''",
-        display: "table",
-      },
-      [`.${e("gutters")}, .${e("gutters-padding")}`]: {
-        "--gutters-x": "0px",
-        "--gutters-y": "0px",
-        "--gutters-x-h": "calc(var(--gutters-x) / 2)",
-        "--gutters-x-h-n": "calc(var(--gutters-x-h) * -1)",
-        "--gutters-y-h": "calc(var(--gutters-y) / 2)",
-        "--gutters-y-h-n": "calc(var(--gutters-y-h) * -1)",
-        margin: "var(--gutters-y-h-n) var(--gutters-x-h-n)",
-      },
-      [`.${e("gutters")} > *`]: {
-        margin: "var(--gutters-y-h) var(--gutters-x-h)",
-      },
-      [`.${e("gutters-padding")} > *`]: {
-        padding: "var(--gutters-y-h) var(--gutters-x-h)",
-      },
-    });
+module.exports = plugin(function ({ theme, variants, addUtilities, e }) {
+  const guttersTheme = theme("gutters");
+  const guttersVariants = variants("gutters");
 
-    const combined = fromPairs(
-      concat(
-        ...map(guttersTheme, (val, mod) => {
-          return [
-            [
-              `.${e(`gutters-${mod}`)}`,
-              {
-                "--gutters-x": String(val) === "0" ? "0px" : val,
-                "--gutters-y": String(val) === "0" ? "0px" : val,
-              },
-            ],
-          ];
-        })
-      )
-    );
+  addUtilities({
+    [`.${e("gutters-wrapper")}`]: {
+      display: "flow-root",
+    },
+    [`.${e("gutters-wrapper")}::before, .${e("gutters-wrapper")}::after`]: {
+      content: "''",
+      display: "table",
+    },
+    [`.${e("gutters")}, .${e("gutters-padding")}`]: {
+      "--gutters-x": "0px",
+      "--gutters-y": "0px",
+      "--gutters-x-h": "calc(var(--gutters-x) / 2)",
+      "--gutters-x-h-n": "calc(var(--gutters-x-h) * -1)",
+      "--gutters-y-h": "calc(var(--gutters-y) / 2)",
+      "--gutters-y-h-n": "calc(var(--gutters-y-h) * -1)",
+      margin: "var(--gutters-y-h-n) var(--gutters-x-h-n)",
+    },
+    [`.${e("gutters")} > *`]: {
+      margin: "var(--gutters-y-h) var(--gutters-x-h)",
+    },
+    [`.${e("gutters-padding")} > *`]: {
+      padding: "var(--gutters-y-h) var(--gutters-x-h)",
+    },
+  });
 
-    const split = fromPairs(
-      concat(
-        ...map(guttersTheme, (v, m) => {
-          return [
-            [
-              `.${e(`gutters-x-${m}`)}`,
-              {
-                "--gutters-x": String(v) === "0" ? "0px" : v,
-              },
-            ],
-            [
-              `.${e(`gutters-y-${m}`)}`,
-              {
-                "--gutters-y": String(v) === "0" ? "0px" : v,
-              },
-            ],
-          ];
-        })
-      )
-    );
+  const combined = fromPairs(
+    concat(
+      ...map(guttersTheme, (val, mod) => {
+        return [
+          [
+            `.${e(`gutters-${mod}`)}`,
+            {
+              "--gutters-x": String(val) === "0" ? "0px" : val,
+              "--gutters-y": String(val) === "0" ? "0px" : val,
+            },
+          ],
+        ];
+      })
+    )
+  );
 
-    addUtilities({...combined, ...split}, guttersVariants);
-  }
+  const split = fromPairs(
+    concat(
+      ...map(guttersTheme, (v, m) => {
+        return [
+          [
+            `.${e(`gutters-x-${m}`)}`,
+            {
+              "--gutters-x": String(v) === "0" ? "0px" : v,
+            },
+          ],
+          [
+            `.${e(`gutters-y-${m}`)}`,
+            {
+              "--gutters-y": String(v) === "0" ? "0px" : v,
+            },
+          ],
+        ];
+      })
+    )
+  );
+
+  addUtilities({...combined, ...split}, guttersVariants);
+}
 );
